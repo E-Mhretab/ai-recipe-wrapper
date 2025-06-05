0d69fe815ff7ac54eb6778279db3f6cfcc67351c
E-Mhretab <302231812@student.rocmondriaan.nl>
2025-06-05 14:35:48 +0200

    Update config.php

--- ai-recipe-wrapper/config/config.php
+++ ai-recipe-wrapper/config/config.php
@@ -4,6 +4,6 @@
 define
 (
     'API_KEY',
-    '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'
+    'your-api-key'
 );
 // In een latere les vervangen
